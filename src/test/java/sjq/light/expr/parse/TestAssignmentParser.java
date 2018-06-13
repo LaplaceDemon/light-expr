b15efac5ea35ba672df116616435946a31e7739d
jackie.sjq <jackie.sjq@alibaba-inc.com>
2018-06-13 18:00:18 +0800

    Modifying the print results of a test case

--- src/test/java/sjq/light/expr/parse/TestAssignmentParser.java
+++ src/test/java/sjq/light/expr/parse/TestAssignmentParser.java
@@ -7,257 +7,255 @@ public class TestAssignmentParser {
         {
             Parser parser = new Parser("x = 1 + 2");
             BaseExpression parse = parser.parse();
-            System.out.println(parse);
+            System.out.println(parser.getExpression() + " ==> " + parse);
         }
         
         {
             Parser parser = new Parser("x = 1 - 2");
             BaseExpression parse = parser.parse();
-            System.out.println(parse);
+            System.out.println(parser.getExpression() + " ==> " + parse);
         }
         
         {
             Parser parser = new Parser("x = 1 * 2");
             BaseExpression parse = parser.parse();
-            System.out.println(parse);
+            System.out.println(parser.getExpression() + " ==> " + parse);
         }
         
         {
             Parser parser = new Parser("x = 1 / 2");
             BaseExpression parse = parser.parse();
-            System.out.println(parse);
+            System.out.println(parser.getExpression() + " ==> " + parse);
         }
         
         {
             Parser parser = new Parser("x = 1 / 2 + 3");
             BaseExpression parse = parser.parse();
-            System.out.println(parse);
+            System.out.println(parser.getExpression() + " ==> " + parse);
         }
         
         {
             Parser parser = new Parser("x = 1 / 2 - 3");
             BaseExpression parse = parser.parse();
-            System.out.println(parse);
+            System.out.println(parser.getExpression() + " ==> " + parse);
         }
         
         {
             Parser parser = new Parser("y = 1*2*3");
             BaseExpression parse = parser.parse();
-            System.out.println(parse);
+            System.out.println(parser.getExpression() + " ==> " + parse);
         }
         
         {
             Parser parser = new Parser("y = 1*2*3*4");
             BaseExpression parse = parser.parse();
-            System.out.println(parse);
+            System.out.println(parser.getExpression() + " ==> " + parse);
         }
         
         {
             Parser parser = new Parser("y = 1*2*3*4*5");
             BaseExpression parse = parser.parse();
-            System.out.println(parse);
+            System.out.println(parser.getExpression() + " ==> " + parse);
         }
         
         {
             Parser parser = new Parser("y = 1*2*3*4*5 + 1*2*3*4");
             BaseExpression parse = parser.parse();
-            System.out.println(parse);
+            System.out.println(parser.getExpression() + " ==> " + parse);
         }
         
         {
             Parser parser = new Parser("x = 1 + 2 * 3");
             BaseExpression parse = parser.parse();
-            System.out.println(parse);
+            System.out.println(parser.getExpression() + " ==> " + parse);
         }
         
         {
             Parser parser = new Parser("x = 1 * 2 + 3 * 4 + 5 * 6");
             BaseExpression parse = parser.parse();
-            System.out.println(parse);
+            System.out.println(parser.getExpression() + " ==> " + parse);
         }
         
         {
             Parser parser = new Parser("x = 1 * 2 + 3 * 4 * 5");
             BaseExpression parse = parser.parse();
-            System.out.println(parse);
+            System.out.println(parser.getExpression() + " ==> " + parse);
         }
         
         {
             Parser parser = new Parser("x = 1 - 2");
             BaseExpression parse = parser.parse();
-            System.out.println(parse);
+            System.out.println(parser.getExpression() + " ==> " + parse);
         }
         
         {
             Parser parser = new Parser("x = 1 - 2 * 3");
             BaseExpression parse = parser.parse();
-            System.out.println(parse);
+            System.out.println(parser.getExpression() + " ==> " + parse);
         }
         
         {
             Parser parser = new Parser("x = 1 - 2 - 3");
             BaseExpression parse = parser.parse();
-            System.out.println(parse);
+            System.out.println(parser.getExpression() + " ==> " + parse);
         }
         
         {
             Parser parser = new Parser("x = 1 * 2 - 3");
             BaseExpression parse = parser.parse();
-            System.out.println(parse);
+            System.out.println(parser.getExpression() + " ==> " + parse);
         }
         
         {
             Parser parser = new Parser("x = 1 * 2 - 3 * 4");
             BaseExpression parse = parser.parse();
-            System.out.println(parse);
+            System.out.println(parser.getExpression() + " ==> " + parse);
         }
         
         {
             Parser parser = new Parser("x = 1 * 2 - 3 * 4 - 5");
             BaseExpression parse = parser.parse();
-            System.out.println(parse);
+            System.out.println(parser.getExpression() + " ==> " + parse);
         }
         
         {
             Parser parser = new Parser("x = 1 * 2 + 3 * 4 * 5");
             BaseExpression parse = parser.parse();
-            System.out.println(parse);
+            System.out.println(parser.getExpression() + " ==> " + parse);
         }
         
         {
             Parser parser = new Parser("x = 1 * 2 + 3 * 4 * 5 + 6");
             BaseExpression parse = parser.parse();
-            System.out.println(parse);
+            System.out.println(parser.getExpression() + " ==> " + parse);
         }
         
         {
             Parser parser = new Parser("x = (1 + 2) * 3");
             BaseExpression parse = parser.parse();
-            System.out.println(parse);
+            System.out.println(parser.getExpression() + " ==> " + parse);
         }
         
         {
             Parser parser = new Parser("x = (1 + 2) * (3 + 4)");
             BaseExpression parse = parser.parse();
-            System.out.println(parse);
+            System.out.println(parser.getExpression() + " ==> " + parse);
         }
         
         {
             Parser parser = new Parser("x = sin(1 + 2) * cos(3 + 4)");
             BaseExpression parse = parser.parse();
-            System.out.println(parse);
+            System.out.println(parser.getExpression() + " ==> " + parse);
         }
         
         {
             Parser parser = new Parser("y = sin(x)");
             BaseExpression parse = parser.parse();
-            System.out.println(parse);
+            System.out.println(parser.getExpression() + " ==> " + parse);
         }
         
         {
             Parser parser = new Parser("y = sin(x+1)");
             BaseExpression parse = parser.parse();
-            System.out.println(parse);
+            System.out.println(parser.getExpression() + " ==> " + parse);
         }
         
         {
             Parser parser = new Parser("y = sin(x+1) + 2");
             BaseExpression parse = parser.parse();
-            System.out.println(parse);
+            System.out.println(parser.getExpression() + " ==> " + parse);
         }
         
         {
             Parser parser = new Parser("y = sin(x+1) + 2*3");
             BaseExpression parse = parser.parse();
-            System.out.println(parse);
+            System.out.println(parser.getExpression() + " ==> " + parse);
         }
         
         {
             Parser parser = new Parser("y = sin(x+1) + 2*3");
             BaseExpression parse = parser.parse();
-            System.out.println(parse);
+            System.out.println(parser.getExpression() + " ==> " + parse);
         }
         
         {
             Parser parser = new Parser("y = sin(x+1) + cos(2*3)");
             BaseExpression parse = parser.parse();
-            System.out.println(parse);
+            System.out.println(parser.getExpression() + " ==> " + parse);
         }
         
         {
             Parser parser = new Parser("y = sin(x+1)");
             BaseExpression parse = parser.parse();
-            System.out.println(parse);
+            System.out.println(parser.getExpression() + " ==> " + parse);
         }
         
         {
             Parser parser = new Parser("y = sin(sin(x))");
             BaseExpression parse = parser.parse();
-            System.out.println(parse);
+            System.out.println(parser.getExpression() + " ==> " + parse);
         }
         
         {
             Parser parser = new Parser("y = sin(a + cos(b))");
             BaseExpression parse = parser.parse();
-            System.out.println(parse);
+            System.out.println(parser.getExpression() + " ==> " + parse);
         }
         
         
         {
             Parser parser = new Parser("y = a + sin(b)");
             BaseExpression parse = parser.parse();
-            System.out.println(parse);
+            System.out.println(parser.getExpression() + " ==> " + parse);
         }
         
         {
             Parser parser = new Parser("y = a * sin(b)");
             BaseExpression parse = parser.parse();
-            System.out.println(parse);
+            System.out.println(parser.getExpression() + " ==> " + parse);
         }
         
         {
             Parser parser = new Parser("y = a / sin(b)");
             BaseExpression parse = parser.parse();
-            System.out.println(parse);
+            System.out.println(parser.getExpression() + " ==> " + parse);
         }
         
         {
             Parser parser = new Parser("y = cos(sin(x))");
             BaseExpression parse = parser.parse();
-            System.out.println(parse);
+            System.out.println(parser.getExpression() + " ==> " + parse);
         }
         
         {
             Parser parser = new Parser("y = cos(sin(a) + b*c + sin(d*e))");
             BaseExpression parse = parser.parse();
-            System.out.println(parse);
+            System.out.println(parser.getExpression() + " ==> " + parse);
         }
         
         {
             Parser parser = new Parser("y = cos(sin(a)*cos(b) + b*c + sin(d*e))");
             BaseExpression parse = parser.parse();
-            System.out.println(parse);
+            System.out.println(parser.getExpression() + " ==> " + parse);
         }
         
         {
             Parser parser = new Parser("y = 'a' + 'b'");
             BaseExpression parse = parser.parse();
-            System.out.println(parse);
+            System.out.println(parser.getExpression() + " ==> " + parse);
         }
         
         {
             Parser parser = new Parser("y = cos('a' + 'b')");
             BaseExpression parse = parser.parse();
-            System.out.println(parse);
+            System.out.println(parser.getExpression() + " ==> " + parse);
         }
         
         {
             Parser parser = new Parser("y = cos('a'*12 + 'b')");
             BaseExpression parse = parser.parse();
-            System.out.println(parse);
+            System.out.println(parser.getExpression() + " ==> " + parse);
         }
-        
-        
         
     }
 }
--- src/test/java/sjq/light/expr/parse/TestExpressionParser.java
+++ src/test/java/sjq/light/expr/parse/TestExpressionParser.java
@@ -7,67 +7,67 @@ public class TestExpressionParser {
         {
             Parser parser = new Parser("a");
             BaseExpression parse = parser.parse();
-            System.out.println(parse);
+            System.out.println(parser.getExpression() + " ==> " + parse);
         }                                                                                                                                                                                                                              
         
         {
             Parser parser = new Parser("obj.x");
             BaseExpression parse = parser.parse();
-            System.out.println(parse);
+            System.out.println(parser.getExpression() + " ==> " + parse);
         }
         
         {
             Parser parser = new Parser("obj1.f1 + obj2.f2");
             BaseExpression parse = parser.parse();
-            System.out.println(parse);
+            System.out.println(parser.getExpression() + " ==> " + parse);
         }
         
         {
             Parser parser = new Parser("max(a, b)");
             BaseExpression parse = parser.parse();
-            System.out.println(parse);
+            System.out.println(parser.getExpression() + " ==> " + parse);
         }
         
         {
             Parser parser = new Parser("min(a, b)");
             BaseExpression parse = parser.parse();
-            System.out.println(parse);
+            System.out.println(parser.getExpression() + " ==> " + parse);
         }
         
         {
             Parser parser = new Parser("min(a+b,c)");
             BaseExpression parse = parser.parse();
-            System.out.println(parse);
+            System.out.println(parser.getExpression() + " ==> " + parse);
         }
         
         {
             Parser parser = new Parser("min(a+b,c,d)");
             BaseExpression parse = parser.parse();
-            System.out.println(parse);
+            System.out.println(parser.getExpression() + " ==> " + parse);
         }
         
         {
             Parser parser = new Parser("min(a+b,c,d+e)");
             BaseExpression parse = parser.parse();
-            System.out.println(parse);
+            System.out.println(parser.getExpression() + " ==> " + parse);
         }
         
         {
             Parser parser = new Parser("min(a+b,c,sin(d+e))");
             BaseExpression parse = parser.parse();
-            System.out.println(parse);
+            System.out.println(parser.getExpression() + " ==> " + parse);
         }
         
         {
             Parser parser = new Parser("cos(a+b),c,sin(d+e)");
             BaseExpression parse = parser.parse();
-            System.out.println(parse);
+            System.out.println(parser.getExpression() + " ==> " + parse);
         }
         
         {
             Parser parser = new Parser("cos(a+b),obj.f1,sin(d+e)");
             BaseExpression parse = parser.parse();
-            System.out.println(parse);
+            System.out.println(parser.getExpression() + " ==> " + parse);
         }
         
     }
